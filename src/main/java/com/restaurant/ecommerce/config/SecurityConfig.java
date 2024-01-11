421492e7341da9177d21813ddc3bac87a97345c6
Zeke-Cachi <ezequielcachi@gmail.com>
2024-01-11 16:20:54 -0300

    oauth: added oauth2 success handler

--- src/main/java/com/restaurant/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/restaurant/ecommerce/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.restaurant.ecommerce.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
@@ -15,27 +18,34 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+  @Autowired
+  private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity httpSec) throws Exception {
     return httpSec
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> {
               auth.requestMatchers("/api/v1/users/register", "/api/v1/pizzas", "/api/v1/users/form").permitAll();
               auth.anyRequest().authenticated();
             })
-            .oauth2Login(withDefaults())
+            .oauth2Login(oauth -> {
+              oauth.successHandler(oAuth2LoginSuccessHandler);
+            })
             .build();
   }
 
   @Bean
-  CorsConfiguration corsConfiguration() {
+  CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(List.of("http://localhost:5843"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowedMethods(List.of("*"));
     configuration.setAllowCredentials(true);
     UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-    urlBasedCorsConfigurationSource.registerCorsConfiguration("/", configuration);
-    return configuration;
+    urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+    return urlBasedCorsConfigurationSource;
   }
 
 }
